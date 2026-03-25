44e8d8a6518c9eff4e305696d189339055cd5c08
Michael Lizzio <mjl2396@rit.edu>
2026-03-25 00:14:55 -0400

    Fix bug 2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,9 +13,21 @@ variable "table_name" {
   default = "Players"
 }
 
+# existing player image bucket
 variable "bucket_name" {
   type    = string
   default = "athlete-photos-team2"
+}
+
+# new user upload bucket
+variable "user_upload_bucket_name" {
+  type    = string
+  default = ""
+}
+
+variable "user_upload_bucket_force_destroy" {
+  type    = bool
+  default = true
 }
 
 variable "allowed_origins" {
@@ -31,6 +43,21 @@ variable "index_lambda_timeout" {
 variable "search_lambda_timeout" {
   type    = number
   default = 30
+}
+
+variable "upload_lambda_timeout" {
+  type    = number
+  default = 30
+}
+
+variable "player_details_lambda_timeout" {
+  type    = number
+  default = 30
+}
+
+variable "player_image_url_expires" {
+  type    = number
+  default = 3600
 }
 
 variable "invoke_index_on_apply" {
